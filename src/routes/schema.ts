d4e23f4c4676282998cd3d27f9d44d2a9cc48c58
Mikael Siidorow <mikael@siidorow.com>
2025-01-23 20:06:31 +0200

    feat: use a cool form library for nicer form experience

---
+++ src/routes/schema.ts
@@ -0,0 +1,9 @@
+import * as z from "zod";
+
+export const schema = z.object({
+	email: z.string().email(),
+	firstNames: z.string().min(1),
+	lastName: z.string().min(1),
+	homeMunicipality: z.string().min(1),
+	isAllowedEmails: z.boolean(),
+});
