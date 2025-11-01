e4a851765f44d451d645cf9f01f473f1b04fe15c
Luukas Pörtfors <lajp@iki.fi>
2025-11-01 17:48:51 +0200

    chore: get rid of devenv

--- flake.nix
+++ flake.nix
@@ -2,94 +2,96 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
-    devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+      ...
+    }@inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          nodejs = pkgs.nodejs_24;
+          pnpm = pkgs.pnpm.override { inherit nodejs; };
 
-  outputs = {
-    self,
-    nixpkgs,
-    devenv,
-    systems,
-    ...
-  } @ inputs: let
-    forEachSystem = nixpkgs.lib.genAttrs (import systems);
-  in {
-    packages = forEachSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      nodejs = pkgs.nodejs_24;
-      pnpm = pkgs.pnpm.override {inherit nodejs;};
+          default = pkgs.stdenv.mkDerivation (finalAttrs: {
+            pname = "rekisteri";
+            version = self.rev or "dirty";
 
-      default = pkgs.stdenv.mkDerivation (finalAttrs: {
-        pname = "rekisteri";
-        version = self.rev or "dirty";
+            src = ./.;
 
-        src = ./.;
+            nativeBuildInputs = [
+              nodejs
+              pnpm
+              pnpm.configHook
+            ];
 
-        nativeBuildInputs = [
-          nodejs
-          pnpm
-          pnpm.configHook
-        ];
+            STRIPE_API_KEY = "sk_test_...";
+            STRIPE_WEBHOOK_SECRET = "whsec_...";
+            DATABASE_URL = "postgres://root:mysecretpassword@localhost:5432/local";
 
-        STRIPE_API_KEY = "sk_test_...";
-        STRIPE_WEBHOOK_SECRET = "whsec_...";
-        DATABASE_URL = "postgres://root:mysecretpassword@localhost:5432/local";
+            pnpmDeps =
+              (pnpm.fetchDeps {
+                name = "${finalAttrs.pname}-${finalAttrs.version}-pnpm-deps";
+                inherit (finalAttrs) pname version src;
+                inherit nodejs;
+                fetcherVersion = 2;
+                hash = "sha256-5g43nThfyw74TJHOkkcjQqWgTaLM7ge38QcwWgm+4xA=";
+              }).overrideAttrs
+                (old: {
+                  nativeBuildInputs = [
+                    nodejs
+                    pnpm
+                  ]
+                  ++ old.nativeBuildInputs;
+                });
 
-        pnpmDeps = (pnpm.fetchDeps {
-          name = "${finalAttrs.pname}-${finalAttrs.version}-pnpm-deps";
-          inherit (finalAttrs) pname version src;
-          inherit nodejs;
-          fetcherVersion = 2;
-          hash = "sha256-nGATXci2zXbyIMUGIhECTIwc4GoP9WgT1iRep5xtWCM=";
-        }).overrideAttrs (old: {
-          nativeBuildInputs = [nodejs pnpm] ++ old.nativeBuildInputs;
-        });
+            installPhase = ''
+              runHook preInstall
+              pnpm build
+              cp -r build $out
+              pnpm prune --prod
+              cp -r node_modules $out
+              runHook postInstall
+            '';
+          });
+        in
+        {
+          inherit default;
+          docker = pkgs.dockerTools.buildLayeredImage {
+            name = "rekisteri";
+            tag = "latest";
+            config.Cmd = [
+              "${nodejs}/bin/node"
+              "${default}/index.js"
+            ];
+          };
+        }
+      );
 
-        installPhase = ''
-          runHook preInstall
-          pnpm build
-          cp -r build $out
-          pnpm prune --prod
-          cp -r node_modules $out
-          runHook postInstall
-        '';
-      });
-    in {
-      inherit default;
-      docker = pkgs.dockerTools.buildLayeredImage {
-        name = "rekisteri";
-        tag = "latest";
-        config.Cmd = ["${nodejs}/bin/node" "${default}/index.js"];
-      };
-      devenv-up = self.devShells.${system}.default.config.procfileScript;
-      devenv-test = self.devShells.${system}.default.config.test;
-    });
-
-    devShells =
-      forEachSystem
-      (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        nodejs = pkgs.nodejs_24;
-        pnpm = pkgs.pnpm.override {inherit nodejs;};
-      in {
-        default = devenv.lib.mkShell {
-          inherit inputs pkgs;
-          modules = [
-            {
-              languages.javascript = {
-                enable = true;
-                pnpm.enable = true;
-                pnpm.package = pnpm;
-              };
-            }
-          ];
-        };
-      });
-  };
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          nodejs = pkgs.nodejs_24;
+          pnpm = pkgs.pnpm.override { inherit nodejs; };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              nodejs
+              pnpm
+            ];
+          };
+        }
+      );
+    };
 }
