0639b8c993a6e0ae4d34e46f99c7881aad4abb46
Joona Kärkkäinen <joona.karkkainen@gmail.com>
2025-11-07 01:31:38 +0200

    chore: narrower param typing

--- src/params/locale.ts
+++ src/params/locale.ts
@@ -1,6 +1,6 @@
 import type { ParamMatcher } from "@sveltejs/kit";
 import { locales, type Locale } from "$lib/i18n/routing";
 
-export const match: ParamMatcher = (param) => {
+export const match = ((param: string): param is Locale => {
 	return locales.includes(param as Locale);
-};
+}) satisfies ParamMatcher;
