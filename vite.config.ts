504de8df80efba698aa0b4cf118daf2eda7c69f1
Mikael Siidorow <mikael@siidorow.com>
2025-11-06 22:02:40 +0200

    chore: sv add vitest for unit and integration testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
 import devtoolsJson from "vite-plugin-devtools-json";
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { kitRoutes } from "vite-plugin-kit-routes";
-import { defineConfig } from "vite";
 import type { KIT_ROUTES } from "$lib/ROUTES";
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit(), kitRoutes<KIT_ROUTES>(), devtoolsJson()],
+	test: {
+		expect: { requireAssertions: true },
+		projects: [
+			{
+				extends: "./vite.config.ts",
+				test: {
+					name: "server",
+					environment: "node",
+					include: ["src/**/*.{test,spec}.{js,ts}"],
+					exclude: ["src/**/*.svelte.{test,spec}.{js,ts}"],
+				},
+			},
+		],
+	},
 });
