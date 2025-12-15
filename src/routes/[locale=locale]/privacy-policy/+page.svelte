c9acc4a580062922578c6a5af74608127ec833b1
Mikael Siidorow <mikael@siidorow.com>
2025-12-15 18:41:45 +0200

    fix: tweak legalese

--- src/routes/[locale=locale]/privacy-policy/+page.svelte
+++ src/routes/[locale=locale]/privacy-policy/+page.svelte
@@ -55,6 +55,11 @@
 			<h2>{$LL.documents.privacyPolicy.section9Title()}</h2>
 			<p>{@html $LL.documents.privacyPolicy.section9Content()}</p>
 		</section>
+
+		<section>
+			<h2>{$LL.documents.privacyPolicy.section10Title()}</h2>
+			<p>{@html $LL.documents.privacyPolicy.section10Content()}</p>
+		</section>
 		<!-- eslint-enable svelte/no-at-html-tags -->
 
 		<p class="mt-8 text-sm text-gray-600 dark:text-gray-400">
