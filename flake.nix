c7d175b141eb446dde0268a245d4a2c23b479945
Mikael Siidorow <mikael@siidorow.com>
2026-01-07 20:17:22 +0200

    chore: update nix pnpm deps sha

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
                 inherit (finalAttrs) pname version src;
                 inherit nodejs;
                 fetcherVersion = 2;
-                hash = "sha256-NJlfBHeMNqv0NXt6e5mogo240OU81jjtM8eA0PvXsQc=";
+                hash = "sha256-ev5j6G3K8OlamHL25bUT5xaoTyX5GoDiHgdifc6dlQo=";
               }).overrideAttrs
                 (old: {
                   nativeBuildInputs = [
