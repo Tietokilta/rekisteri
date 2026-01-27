dad2c74d306145c6f490cfd272eaff8a69b67568
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2026-01-27 23:37:27 +0200

    fix: update pnpm deps hash for testcontainers

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
                 inherit (finalAttrs) pname version src;
                 inherit nodejs;
                 fetcherVersion = 2;
-                hash = "sha256-V/+di/hVswYf4nEg5iJFg5/5KMYmxIVUpgUZe1FEjyw=";
+                hash = "sha256-8htvXrSOt2cYRDyiUuQnXqM0cMFqFN0hqPGaxsFWYSY=";
               }).overrideAttrs
                 (old: {
                   nativeBuildInputs = [
