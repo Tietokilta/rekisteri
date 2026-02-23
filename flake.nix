1e18e61e228e81abcf3cea56aa52ebc8f163cfb3
Mikael Siidorow <mikael@siidorow.com>
2026-02-23 19:01:45 +0200

    chore: add prettier plugin oxc

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
                 inherit (finalAttrs) pname version src;
                 inherit nodejs;
                 fetcherVersion = 2;
-                hash = "sha256-IeQX994/3nVg9xnBCv7uKmHK8l83E3bfSbk2rkZIoVY=";
+                hash = "sha256-UVCgsT/UfeNb2tBkRiV6ScQffh5J9BabqGiBxPFM/2Y=";
               }).overrideAttrs
                 (old: {
                   nativeBuildInputs = [
