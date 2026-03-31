a6ffc696a32ac0ca9ac3ec78ac4b99d0cf070e7e
Akseli Pihlajamaa <akselijuhanipihlajamaa@gmail.com>
2026-03-31 18:12:54 +0300

    create e2e test for customise page

--- src/routes/[locale=locale]/(app)/admin/customise/+page.svelte
+++ src/routes/[locale=locale]/(app)/admin/customise/+page.svelte
@@ -499,6 +499,7 @@
           <button
             type="submit"
             class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-none"
+            data-testid="save-customisations"
           >
             {$LL.admin.customise.save()}
           </button>
