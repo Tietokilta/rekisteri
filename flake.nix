b14223682c35c07d306974148651ca27a085778d
Mikael Siidorow <mikael@siidorow.com>
2026-06-03 20:36:42 +0300

    chore: update flake.nix hash

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
                 inherit (finalAttrs) pname version src;
                 inherit nodejs;
                 fetcherVersion = 2;
-                hash = "sha256-7o9YGyKZS6NFTJRn9Y3XiQE2h+VY7uVSo4NxaHc0TOc=";
+                hash = "sha256-Ql4kiXCPBhya4rKtW9dF+G4bO1Z1LZAJhX4c/4Txn9c=";
               }).overrideAttrs
                 (old: {
                   nativeBuildInputs = [
